984b3c5d7d11ba4ff30035824e8620f2e45d9cc9
mathaussilvaj <mathaussilva7@gmail.com>
2024-05-11 14:23:51 +0200

    summary

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const texts = ["Data Scientist", "AI Enthusiast", "LLMs", "Automation"];
+    let index = 0;
+    const carouselText = document.getElementById("carousel-text");
+
+    function updateText() {
+        carouselText.textContent = texts[index];
+        index = (index + 1) % texts.length;
+    }
+
+    setInterval(updateText, 2000); // Match the animation duration
+});
